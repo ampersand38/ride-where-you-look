6dfa052f967d21fdf8b92c966587887ea9813f14
ampersand38 <ampersand38@gmail.com>
2020-07-04 02:54:07 -0400

    comment

--- addons/main/XEH_PREP.hpp
+++ addons/main/XEH_PREP.hpp
@@ -1,6 +1,5 @@
 // testing, update fncs on the fly
 //#define PREP(var1) TRIPLES(ADDON,fnc,var1) = { call compile preProcessFileLineNumbers '\MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\functions\DOUBLES(fnc,var1).sqf' }
-//amp_slingload_fnc_adjustRigging = {call compile preProcessFileLineNumbers '\z\amp\addons\slingload\functions\fnc_adjustRigging.sqf'}
 
 PREP(findSeat);
 PREP(moveSeat);
