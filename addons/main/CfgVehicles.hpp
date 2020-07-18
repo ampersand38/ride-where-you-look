0e4934e6bab368173e38a46eeec59105992f672a
ampersand38 <ampersand38@gmail.com>
2020-07-17 23:37:18 -0400

    added VTOL vehicle transport blacklists

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -7,4 +7,68 @@ class CfgVehicles {
             "proxy:\a3\data_f\proxies\gunner_standup01\cargo.006"
         };
     };
+
+    class VTOL_01_vehicle_base_F;
+    class B_T_VTOL_01_vehicle_F: VTOL_01_vehicle_base_F {
+        RWYL_proxyBlacklist[] = {
+            "proxy:\a3\data_f\proxies\heli_transport_01\cargo.001",
+            "proxy:\a3\data_f\proxies\heli_transport_01\cargo.002",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic04\cargo.003",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo.004",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic02\cargo.005",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic01\cargo.006",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic04\cargo.007",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo.008",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic02\cargo.009",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic01\cargo.010",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic01\cargo.011",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic03\cargo.012",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic04\cargo.013",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic01\cargo.014",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.015",
+            "proxy:\a3\data_f\proxies\passenger_generic01_foldhands\cargo.016",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.017",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic03\cargo.018",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic02\cargo.019",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.020",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic02\cargo.023",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic01\cargo.024",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic01\cargo.025",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic03\cargo.026",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic04\cargo.027",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic01\cargo.028",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.029",
+            "proxy:\a3\data_f\proxies\passenger_generic01_foldhands\cargo.030",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.031",
+            "proxy:\a3\data_f\proxies\passenger_apc_narrow_generic03\cargo.032",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic02\cargo.033",
+            "proxy:\a3\data_f\proxies\passenger_apc_generic03\cargo.034"
+        };
+    };
+
+    class O_T_VTOL_02_vehicle_F: O_T_VTOL_02_vehicle_base_F {
+        RWYL_proxyBlacklist[] = {
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.001",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.002",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.003",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.004",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.005",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.006",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.007",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.008",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.009",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.010",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.011",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.012",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.013",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.014",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.015",
+            "proxy:\a3\data_f\proxies\passenger_generic01_leanleft\cargo01.016"
+        };
+    };
 };
+
+/*
+_v = ([[curatorSelected # 0 # 0, [vehicle player, cursorObject] select (vehicle player == player)] select isNull curatorCamera, (get3DENSelected "" # 0 # 0)] select is3DEN);
+selectionNames _v select {"cargo" in toLower _x}
+*/
