3793618bb6aa3c46814f70feb62699fdee46fb4f
ampersand38 <ampersand38@gmail.com>
2020-08-12 14:11:29 -0400

    Blacklist LSV armed rear seats

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -1,5 +1,11 @@
 class CfgVehicles {
     class LSV_01_base_F;
+    class LSV_01_armed_base_F: LSV_01_base_F {
+        RWYL_proxyBlacklist[] = {
+            "proxy:\a3\data_f\proxies\passenger_low01\cargo.004",
+            "proxy:\a3\data_f\proxies\passenger_low01\cargo.005"
+        };
+    };
     class LSV_01_AT_base_F: LSV_01_base_F {
         RWYL_proxyBlacklist[] = {
             "proxy:\a3\data_f\proxies\passenger_low01\cargo.004",
