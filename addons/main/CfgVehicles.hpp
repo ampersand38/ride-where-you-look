42a2a96c5b2699022fb0f2ef914ea0b6a47613c1
ampersand38 <ampersand38@gmail.com>
2020-08-14 23:59:18 -0400

    Blacklist LSV armed center FFV

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -3,7 +3,8 @@ class CfgVehicles {
     class LSV_01_armed_base_F: LSV_01_base_F {
         RWYL_proxyBlacklist[] = {
             "proxy:\a3\data_f\proxies\passenger_low01\cargo.004",
-            "proxy:\a3\data_f\proxies\passenger_low01\cargo.005"
+            "proxy:\a3\data_f\proxies\passenger_low01\cargo.005",
+            "proxy:\a3\data_f\proxies\gunner_standup01\cargo.006"
         };
     };
     class LSV_01_AT_base_F: LSV_01_base_F {
